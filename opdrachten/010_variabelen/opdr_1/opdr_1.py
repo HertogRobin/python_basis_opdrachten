0e696a63f6b4e9528170a78d70762c26bdca030d
HertogRobin <robin@robin-ict.com>
2023-09-21 12:07:27 +0200

    opdr_1

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,7 +1,14 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Robin van de Wetering
 # Groep:
 
 # Hier komt je code...
 
-my_list = ...
+getal = 13
+zin = "wat een weertje"
+getal2 = 12.5
+getal3 = 8
+print (getal)
+print (zin)
+print (getal2)
+print (getal3)
