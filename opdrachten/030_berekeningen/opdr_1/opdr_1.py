4e42a79963d8e8b928e6dfd6c2a6faa385545e2e
HertogRobin <robin@robin-ict.com>
2023-10-05 10:41:04 +0200

    opdr 1 en 3

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,18 @@
 # Groep:
 
 # Hier komt je code...
+# Maak variabelen voor je beste vrienden en hun leeftijden
+joe = 23
+bob = 15
+fred = 22
+
+# Bereken de totale waarde van alle leeftijden bij elkaar opgeteld
+totale_leeftijd = joe + bob + fred
+
+# Bereken de gemiddelde leeftijd
+gemiddelde_leeftijd = totale_leeftijd / 3
+
+# Geef de resultaten weer
+print("Dit weten we over joe, bob en fred:")
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =", totale_leeftijd)
+print("De gemiddelde leeftijd van deze boys is:", gemiddelde_leeftijd)
--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -3,14 +3,21 @@
 # Groep:
 
 # Hier komt je code...
-x = 1
-y = ...
-print( ... )
+# Functie om y te berekenen
+def bereken_y(x):
+    y = 4 * x**3 - 2 * x**2 - 1
+    return y
 
-x = 2
-y = ...
-print( ... )
+# Testen met verschillende waarden van x
+x1 = 1
+y1 = bereken_y(x1)
+print(f"Voor x = {x1}, is de uitkomst: {y1}")
 
-x = 0
-y = ...
-print( ... )
+x2 = 2
+y2 = bereken_y(x2)
+print(f"Voor x = {x2}, is de uitkomst: {y2}")
+
+x3 = 0
+y3 = bereken_y(x3)
+print(f"Voor x = {x3}, is de uitkomst: {y3}")
+
